e7aec8bfa45dbd36f071792dd514357f01ff158b
kentny <kentaro.terasaki@gmail.com>
2023-01-14 11:44:45 +0900

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,21 @@ import React, { useEffect, useState } from 'react';
 function App() {
     const [hello, setHello] = useState()
 
-    useEffect(() => {
-        (async () => {
+    return <>
+        <h1>
+            Hello App
+        </h1>
+        <button onClick={async () => {
             const response = await fetch(`${process.env.REACT_APP_API_SERVER}/hello`, { mode: 'cors' })
             const text = (await response.json()).text
             setHello(text)
-        })()
-    }, [])
-
-    return <div>
-        {hello}
-    </div>
+        }}>
+            Call API
+        </button>
+        <div>
+            {hello}
+        </div>
+    </>
 }
 
 export default App;
